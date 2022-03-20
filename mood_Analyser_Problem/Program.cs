7712b481dab393196cd4dba738be48248dbc2511
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-20 22:00:52 +0530

    Happy or Sad

---
+++ mood_Analyser_Problem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Mood_Analyser_Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Mood analyser program.");
+        }
+    }
+}
